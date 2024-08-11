83e008c430b1a0231d0b63fe5d28260a384612b7
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-08-12 00:03:32 +0100

    task 4 and 5

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 --  lists all bands with Glam rock as their main style, ranked by their longevity
 
-SELECT band_name, (2022 - formed) AS lifespan FROM metal_bands
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
