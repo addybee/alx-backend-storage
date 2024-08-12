88145432f7a61fe1e1ef4f2b2a81436352564ae1
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-08-12 01:22:37 +0100

    task6

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,27 @@
+--  adds a new correction for a student.
+
+DELIMITER //
+
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(16), IN score INT)
+BEGIN
+    DECLARE project_id INT;
+    
+    -- Find the project ID by project name
+    SELECT id INTO project_id
+    FROM projects
+    WHERE name = project_name;
+    
+    -- Check if the project name exists
+    IF project_id IS NULL THEN
+        -- If not found, insert a default project
+        INSERT INTO projects (name) VALUES ('new project');
+        -- Get the ID of the newly inserted default project
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+    
+    -- Insert the new correction
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+
+END //
+
+DELIMITER ;
