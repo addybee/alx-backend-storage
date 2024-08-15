d4c6a6f210f0543533ff6a11d01a0a5eac87b0aa
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-08-15 01:37:42 +0100

    first commit

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Module for caching data using Redis."""
+
+import redis
+from typing import Union, Callable, Optional, Any
+from uuid import uuid4
+
+
+class Cache:
+    """Class for caching data."""
+
+    def __init__(self) -> None:
+        """Initialize the Cache class."""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store data in the cache.
+
+        Args:
+            data (Union[str, bytes, int, float]): Data to be stored.
+
+        Returns:
+            str: The key under which the data is stored.
+        """
+        key: str = str(uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Optional[Callable[[Any], Any]]) -> Any:
+        """Retrieve data from cache and apply a function if provided."""
+        result: Any = self._redis.get(key)
+        return result if fn is None or result is None else fn(result)
+        
+    def get_str(self, key: str) -> Union[str, None]:
+        """Retrieve a string value from cache."""
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """Retrieve an integer value from cache."""
+        return self.get(key, int)
+
